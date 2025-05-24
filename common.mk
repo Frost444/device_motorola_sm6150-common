413e13bd95b1fd756c4e8918bc699a1200ec1983
Thiago <thiaguin2374@gmail.com>
2025-05-24 15:14:33 -0300

    sm6150-common: Remove unused RFS MDM MPSS symlink

--- common.mk
+++ common.mk
@@ -457,10 +457,6 @@ PRODUCT_ODM_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qmi_fw.conf:$(TARGET_COPY_OUT_VENDOR)/etc/qmi_fw.conf
 
-# RFS MSM MPSS symlinks
-PRODUCT_PACKAGES += \
-    rfs_msm_mpss_readonly_vendor_fsg_symlink
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
