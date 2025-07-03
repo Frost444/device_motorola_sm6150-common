1e91df8451f7321f01db023dec2cb9f603e9a61c
Thiago <thjr5965@gmail.com>
2025-07-03 09:25:01 -0300

    sm6150-common: rebringup opt

--- common.mk
+++ common.mk
@@ -239,40 +239,25 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor \
     libutils.vendor
 
-# Android GO tunning
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-USE_DEX2OAT_DEBUG := false
-WITH_DEXPREOPT_DEBUG_INFO := false
-PRODUCT_OTHER_JAVA_DEBUG_INFO := false
-
-# Do not generate libartd.
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-
-# Strip the local variable table and the local variable type table to reduce
-# the size of the system image. This has no bearing on stack traces, but will
-# leave less information available via JDWP.
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-
-# Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
-# Reduce SystemServer Debug
-PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
-
-# Updater
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.recovery_update=false
-
-# Mobile data
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.android.mobiledata=false
-
 # Dexpreopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    Launcher3QuickStep \
     SystemUI \
+    SystemUIGoogle \
     Settings
+
+# Dexpreopt
+WITH_DEXPREOPT_DEBUG_INFO := false
+
+# Bruh
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
+
+# Reduce system server verbosity.
+PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
+PRODUCT_OTHER_JAVA_DEBUG_INFO := false
+
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
