9ec74cccc2b0f98821766473bfbcb9dc34ced1ea
Frost444 <thiagojurema20@gmail.com>
2025-07-09 21:42:17 -0300

    sm6150-common: switch to pixel libperfmgr

--- common.mk
+++ common.mk
@@ -412,7 +412,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
+    android.hardware.power-service.pixel-libperfmgr \
     libqti-perfd-client
 
 # QCOM
@@ -494,7 +494,6 @@ PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
