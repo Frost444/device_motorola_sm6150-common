7ebdb3dd1b28ad61427ad24bdf89fe4f738ec5d2
Frost444 <thiagojurema20@gmail.com>
2025-07-21 09:15:14 -0300

    Revert "sm6150-common: switch to pixel libperfmgr"
    This reverts commit 9ec74cccc2b0f98821766473bfbcb9dc34ced1ea.

--- common.mk
+++ common.mk
@@ -413,7 +413,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
 # QCOM
@@ -495,6 +495,7 @@ PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
