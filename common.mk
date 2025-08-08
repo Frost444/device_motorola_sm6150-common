184a7dda57faadebabecc024bc1a872ad4186ab4
Frost444 <thiagojurema20@gmail.com>
2025-08-07 22:17:03 -0300

    buth

--- common.mk
+++ common.mk
@@ -276,7 +276,6 @@ TARGET_VNDK_USE_CORE_VARIANT := true
 
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
 
 # DebugFS
 PRODUCT_ENABLE_UFFD_GC := false
