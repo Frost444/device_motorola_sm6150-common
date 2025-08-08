e87ccad40fa2682db6dbb1ef92129c31726e5468
Frost444 <thiagojurema20@gmail.com>
2025-08-07 22:11:48 -0300

    Set debug.sf.auto_latch_unsignaled to 0

--- properties.mk
+++ properties.mk
@@ -185,7 +185,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.latch_unsignaled=1 \
+    debug.sf.latch_unsignaled=0 \
     debug.sf.enable_adpf_cpu_hint=true \
     persist.demo.hdmirotationlock=false \
     persist.sys.sf.color_saturation=1.0 \
