93e88d8dccad760db329af8b80d8ad35e3b45d02
Frost444 <thiagojurema20@gmail.com>
2025-08-08 18:14:32 -0300

    malloc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -165,7 +165,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(VENDOR_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/infinity/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE += $(VENDOR_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(VENDOR_PATH)/manifest.xml
 
@@ -176,6 +176,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_sm6150
 # Jemalloc
 MALLOC_SVELTE := true
 MALLOC_SVELTE_FOR_LIBC32 := true
+MALLOC_SVELTE_FOR_LIBC64 := true
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
