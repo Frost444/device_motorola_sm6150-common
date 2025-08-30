732cf88b6e63c0ead8e437ca0262033a230d6e71
FrostLab81 <thiagodd334@gmail.com>
2025-08-30 13:00:01 -0300

    use hardware/motorola

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -237,7 +237,6 @@ TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include hardware/motorola/sepolicy/qti/SEPolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
