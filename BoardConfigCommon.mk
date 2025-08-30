a83cc1e91c7718090300b4ea96655f9043c0774f
FrostLab81 <thiagodd334@gmail.com>
2025-08-30 15:22:44 -0300

    Revert "test"
    This reverts commit 11929e9afa3a275a85e8c125705d6659a6214ef7.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -233,7 +233,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
-include hardware/motorola/sepolicy/qti/SEPolicy.mk
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
 # Android Verified Boot
