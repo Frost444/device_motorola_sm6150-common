4af47d56aec2f0aa8de0dcaab3ce329ece6b700c
FrostLab81 <thiagodd334@gmail.com>
2025-08-30 15:22:28 -0300

    Revert "use hardware/motorola"
    This reverts commit 732cf88b6e63c0ead8e437ca0262033a230d6e71.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -234,6 +234,7 @@ TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include hardware/motorola/sepolicy/qti/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
