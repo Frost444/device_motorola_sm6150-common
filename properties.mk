b99d322cffc568b3ebb702caf9c823e57c3bdd72
Frost444 <thiagojurema20@gmail.com>
2026-06-23 01:45:16 -0300

    sm6150-common: Disable SDM rotator downscaler Rotator down scaler is causing additional delays that are better handled by SDE/GL composition.

--- properties.mk
+++ properties.mk
@@ -173,6 +173,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_optimize_refresh=1 \
     vendor.display.use_smooth_motion=1 \
     vendor.display.disable_offline_rotator=1 \
+    vendor.display.disable_rotator_downscale=1 \
     vendor.display.disable_hw_recovery_dump=1
 
 # DRM
